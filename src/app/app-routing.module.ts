298573c55dec8d42dced128f6e0a5707d40ad065
ani devdariani <101930091+devdariannianni@users.noreply.github.com>
2022-12-09 16:21:03 +0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { Homework1Component } from './homework1/homework1.component';
+import { FormComponent } from './review/form/form.component';
+import { ProductComponent } from './shoping-cart/product/product.component';
+import { TodoListComponent } from './todo-app/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CardComponent,
+    
+  },
+  {
+    path: 'products',
+    component: ProductComponent,
+    
+  },
+  {
+    path: 'todo',
+    component: TodoListComponent
+  },
+  {
+    path: 'home',
+    redirectTo: ''
+  },
+  {
+    path: 'homework.1',
+    component: Homework1Component
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
